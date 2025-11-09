c76dfa1e67c785542f85becc8b68051de4256647
Schubert Rodriguez <schubert.david.r@gmail.com>
2025-11-09 18:51:49 +0000

    fix(task-0038): update getCarById to use ParseIntPipe for parameter validation

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -11,7 +11,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.carsService.findOneById(Number(id));
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
   }
 }
