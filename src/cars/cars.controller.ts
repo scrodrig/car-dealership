28cfa24eee646f878852c2553385dd83ac08ccb3
Schubert Rodriguez <schubert.david.r@gmail.com>
2025-11-09 18:31:56 +0000

    feat(task-0035): cars - implement CarsController with car retrieval methods and add BRU files for API endpoints

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = [
+    { id: 1, make: 'Toyota', model: 'Corolla' },
+    { id: 2, make: 'Honda', model: 'Civic' },
+    { id: 3, make: 'Renault', model: 'Captur' },
+  ];
+
   @Get()
   getAllCars() {
-    return [
-      { id: 1, make: 'Toyota', model: 'Corolla' },
-      { id: 2, make: 'Honda', model: 'Civic' },
-      { id: 3, make: 'Renault', model: 'Captur' },
-    ];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    return this.cars.find((car) => car.id === +id);
   }
 }
