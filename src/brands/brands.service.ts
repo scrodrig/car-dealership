3c31045df3735d0732948a512c332769f903a79b
Schubert Rodriguez Quezada <schubert.quezada@deutschefintechsolutions.de>
2025-11-24 17:04:45 +0000

    feat(task-0059): add brand module

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+  private brands: Brand[] = [
+    {
+      id: uuidv4(),
+      name: 'Toyota',
+      createdAt: new Date().getTime(),
+    },
+  ];
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+    const brand: Brand = {
+      id: uuidv4(),
+      ...createBrandDto,
+      createdAt: new Date().getTime(),
+    };
+
+    this.brands.push(brand);
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find((brand) => brand.id === id);
+    if (!brand) throw new NotFoundException(`Brand with ${id} not found`);
+    return brand;
   }
 
-  update(id: number, updateBrandDto: UpdateBrandDto) {
-    return `This action updates a #${id} brand`;
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDB = this.findOne(id);
+    this.brands.map((car) => {
+      if (car.id === id) {
+        brandDB = {
+          ...brandDB,
+          ...updateBrandDto,
+          id,
+          updatedAt: new Date().getTime(),
+        };
+        return brandDB;
+      }
+      return car;
+    });
+
+    return brandDB;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} brand`;
+  remove(id: string) {
+    return this.brands.filter((brand) => brand.id !== id);
   }
 }
